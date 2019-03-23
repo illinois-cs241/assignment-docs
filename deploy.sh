14700a9971965c53e6be3a53376ebd6f66dc15f7
bhuvy <bhuvan.venkatesh21@gmail.com>
2019-03-23 12:06:43 -0500

    Changing deploy to empty commit

--- deploy.sh
+++ deploy.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
 
-git clone --recurse-submodules -j8 -b develop --depth 1 git@github.com:illinois-cs241/illinois-cs241.github.io.git ${CLONE_DIR}
-cd ${CLONE_DIR}
-git checkout develop
-cd _docs/
-git pull origin master
+set -o errexit;
+
 export DOCS_SHA=$(git rev-parse --short HEAD)
-cd ..
-git add _docs
-git commit -m "Updating docs to ${DOCS_SHA}"
+git clone --recurse-submodules -j8 -b develop --depth 1 git@github.com:illinois-cs241/illinois-cs241.github.io.git ${CLONE_DIR}
+pushd ${CLONE_DIR}
+git checkout develop
+git commit --allow-empty -m "Updating docs to ${DOCS_SHA}"
 git push origin develop
+popd
